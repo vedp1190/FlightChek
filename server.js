8b4ea6363ff10b09c123e61d98d869f89fb800d6
Ved Patel <109630353+vedp1190@users.noreply.github.com>
2023-05-08 18:46:51 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const PORT = 8000
+const axios = require('axios').default
+const express = require('express')
+const cors = require('cors')
+const { response } = require('express')
+require('dotenv').config
+
+const app = express()
+app.use(cors())
+app.get('/flights', (req,res) => {
+    const options = {
+        url: `${process.env.URL}?page-size=6`,
+        headers: {
+            accept: 'application/json',
+            'X-Cassandra-Token': process.env.TOKEN
+
+        }
+    }
+    axios.request(options).then(response => {
+        console.log(response.data)
+        res.json(respone.data)
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+app.listen(PORT, () => console.log('running on port ' + PORT))
+
